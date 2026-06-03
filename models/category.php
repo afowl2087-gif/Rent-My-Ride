cb3b429c93e0a8aa028dbdf42eaee1ac0ccf4693
Trizou <smylodon@hotmail.fr>
2026-06-03 15:21:14 +0200

    Create Category model with CRUD operations

---
+++ models/category.php
@@ -0,0 +1,86 @@
+<?php
+
+require_once __DIR__ . '/../config/database.php';
+
+class Category
+{
+    private $pdo;
+
+    private $Id_categories;
+    private $nom_categorie;
+
+    public function __construct()
+    {
+        $db = new Database();
+        $this->pdo = $db->getConnection();
+    }
+
+    // GETTERS
+    public function getId()
+    {
+        return $this->Id_categories;
+    }
+
+    public function getName()
+    {
+        return $this->nom_categorie;
+    }
+
+    // SETTERS
+    public function setName($name)
+    {
+        $this->nom_categorie = $name;
+    }
+
+    // INSERT
+    public function insert()
+    {
+        $sql = "INSERT INTO categories (nom_categorie) VALUES (:name)";
+        $stmt = $this->pdo->prepare($sql);
+
+        return $stmt->execute([
+            'name' => $this->nom_categorie
+        ]);
+    }
+
+    // GET ALL
+    public function getAll()
+    {
+        $sql = "SELECT * FROM categories";
+        $stmt = $this->pdo->query($sql);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // UPDATE
+    public function update($id, $name)
+    {
+        $sql = "UPDATE categories SET nom_categorie = :name WHERE Id_categories = :id";
+        $stmt = $this->pdo->prepare($sql);
+
+        return $stmt->execute([
+            'id' => $id,
+            'name' => $name
+        ]);
+    }
+
+    // DELETE
+    public function delete($id)
+    {
+        $sql = "DELETE FROM categories WHERE Id_categories = :id";
+        $stmt = $this->pdo->prepare($sql);
+
+        return $stmt->execute([
+            'id' => $id
+        ]);
+    }
+
+    // IS EXIST (bonus)
+    public function isExist($name)
+    {
+        $sql = "SELECT * FROM categories WHERE nom_categorie = :name";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute(['name' => $name]);
+
+        return $stmt->rowCount() > 0;
+    }
+}
