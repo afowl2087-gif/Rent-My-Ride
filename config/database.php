79fa7544c5e3b1f151f6db2dcdd7a033259707a3
Trizou <smylodon@hotmail.fr>
2026-06-03 15:22:25 +0200

    database connection management

---
+++ config/database.php
@@ -0,0 +1,31 @@
+<?php
+
+class Database
+{
+    private $host = "localhost";
+    private $db_name = "RentMyRide";
+    private $username = "root";
+    private $password = "";
+
+    public $pdo;
+
+    public function getConnection()
+    {
+        $this->pdo = null;
+
+        try {
+            $this->pdo = new PDO(
+                "mysql:host={$this->host};dbname={$this->db_name};charset=utf8",
+                $this->username,
+                $this->password
+            );
+
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            die("Erreur connexion DB : " . $e->getMessage());
+        }
+
+        return $this->pdo;
+    }
+}
